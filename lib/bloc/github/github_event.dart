2700187f4dbe586ce5dd6cb936a0563efd510ee8
Davin Reinaldo G <davinreinaldogozali@gmail.com>
2022-02-05 00:54:29 +0700

    remove unused package and add kDebugMode print console

--- lib/bloc/github/github_event.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'github_event.freezed.dart';
-
-@freezed
-class GithubEvent with _$GithubEvent {
-  const factory GithubEvent.addIssue(String issue) = _Added;
-}
--- lib/data/interceptor.dart
+++ lib/data/interceptor.dart
@@ -1,31 +1,40 @@
 
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 import 'github/constants.dart';
 
 class HttpInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    print('REQUEST[${options.method}] => PATH: ${options.path} , baseUrl : ${options.baseUrl} entity : ${options.data}');
+    if (kDebugMode) {
+      print('REQUEST[${options.method}] => PATH: ${options.path} , baseUrl : ${options.baseUrl} entity : ${options.data}');
+    }
     if(options.baseUrl == 'https://api.github.com/') {
       options.headers['Authorization'] = 'Bearer $githubToken';
       options.headers['Accept'] = 'application/vnd.github.v3+json';
     }
-    print('header : ${options.headers} entity : ${options.data}');
+    if (kDebugMode) {
+      print('header : ${options.headers} entity : ${options.data}');
+    }
     return super.onRequest(options, handler);
 
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    print('RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
-    print('RESPONSE[${response.statusCode}] => BODY:\n ${response.data}');
+    if (kDebugMode) {
+      print('RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
+      print('RESPONSE[${response.statusCode}] => BODY:\n ${response.data}');
+    }
     super.onResponse(response, handler);
   }
 
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
-    print('ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path} data: ${err.requestOptions.data}');
+    if (kDebugMode) {
+      print('ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path} data: ${err.requestOptions.data}');
+    }
     // TODO: handle network error
     switch(err.response?.statusCode) {
       case 403:
