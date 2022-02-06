a7b87488a5037a38d311110b3fc59ea057bd38ab
Davin Reinaldo G <davinreinaldogozali@gmail.com>
2022-02-06 13:14:26 +0700

    refactor data layer to make it match for clean architecture design

--- lib/data/github/feedback_repository_github_impl.dart
+++ lib/data/github/feedback_repository_github_impl.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_clean_architecture_template/data/github/remote/github_rest_client_config.dart';
 import 'package:flutter_clean_architecture_template/data/github/remote/service/github_rest_api_client.dart';
+import 'package:flutter_clean_architecture_template/data/github/utils/github_mapper.dart';
 import 'package:flutter_clean_architecture_template/domain/feedback/entities/issue.dart';
 import 'package:flutter_clean_architecture_template/domain/feedback/i_feedback_repository.dart';
 import 'package:injectable/injectable.dart';
@@ -13,7 +14,7 @@ class FeedbackRepositoryGithubImpl extends IFeedbackRepository {
   @override
   Future createAndIssue(Issue issue) async {
     print('create and issue $issue');
-    final response = await client.createAndIssue(issue);
+    final response = await client.createAndIssue(GithubMapper.fromIssue(issue));
     print('response : $response');
     return response;
   }
--- lib/data/gitlab/remote/service/gitlab_rest_api_client.dart
+++ lib/data/gitlab/remote/service/gitlab_rest_api_client.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_clean_architecture_template/data/github/remote/responses/issue_response.dart';
-import 'package:flutter_clean_architecture_template/domain/feedback/entities/issue.dart';
+import 'package:flutter_clean_architecture_template/data/gitlab/remote/request/create_issue_request.dart';
 import 'package:retrofit/http.dart';
 import 'package:dio/dio.dart';
 
@@ -13,6 +13,6 @@ abstract class GitlabRestApiClient {
   factory GitlabRestApiClient(Dio dio, {String baseUrl}) = _GitlabRestApiClient;
 
   @POST('projects/$gitlabProjectId/issues')
-  Future<IssueResponse> createAndIssue(@Body() Issue issue);
+  Future<IssueResponse> createAndIssue(@Body() CreateIssueRequest issue);
 
 }
